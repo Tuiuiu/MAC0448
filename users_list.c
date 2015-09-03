ad4c6f17f47248888bf5f072aecfb0c47c953c33
Gabriel Ferreira Guilhoto <gabriel.guilhoto@usp.br>
2015-09-03 20:03:59 -0300

    começando a implementar o JOIN (com problemas)

--- users_list.c
+++ users_list.c
@@ -5,10 +5,13 @@
 
 User *list_init() 
 {
+	int i;
+
 	User *ini;
 	ini = malloc(sizeof (User));
 	ini->connfd = -1;
-	ini->channel = -1;
+	for (i = 0; i < NUMBER_OF_CHANNELS; i++)
+		ini->is_in_channel[i] = false;
 	ini->id = -1;
 	ini->next = NULL;
 	return ini;
@@ -19,7 +22,6 @@ User *create(User *list, int usr_connfd, int usr_channel, int usr_id, nick usr_n
 	User *new_user;
 	new_user = malloc(sizeof (User));
 	new_user->connfd = usr_connfd;
-	new_user->channel = usr_channel;
 	new_user->id = usr_id;
 	strcpy(new_user->nickname, usr_nickname);
 	new_user->next = list->next;
--- users_list.h
+++ users_list.h
@@ -1,10 +1,14 @@
+#include <stdbool.h>
+
 #define MAX_NICK_SIZE 50
+
+#define NUMBER_OF_CHANNELS 2
 
 typedef char nick[MAX_NICK_SIZE];
 
 typedef struct cel {
 	int			connfd;
-	int 		channel;
+	bool		is_in_channel[NUMBER_OF_CHANNELS]; /* is_in_channel[i] é true se o usuário estiver no canal i */
 	int 		id;
 	nick 		nickname;
 	struct cel 	*next;
